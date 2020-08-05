9dd4e6413bcc1e6f029bfa77e6ba1e7b924b8af5
Rhemi7 <adelefatai@yahoo.com>
2020-08-05 02:33:25 +0100

    Splash screen created, assets and some configurations

---
+++ lib/splashscreen.dart
@@ -0,0 +1,53 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    splashTimer();
+  }
+
+  splashTimer() async {
+    var duration = Duration(seconds: 3);
+
+    return new Timer(duration, () {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return initScreen(context);
+  }
+
+  initScreen(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF4545C5),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: Image.asset('images/Logo.png'),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              'MyNetWorth',
+              style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Open Sans',
+                  fontSize: 32,
+                  fontWeight: FontWeight.w600),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}

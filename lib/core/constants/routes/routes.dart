7615b9f58ec05e5ffd970c71345c8601ee057062
Rhemi7 <adelefatai@yahoo.com>
2020-08-05 15:35:44 +0100

    Add Item Screen

--- lib/core/constants/routes/routes.dart
+++ lib/core/constants/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:networthapp/ui/add_item_screen.dart';
 import 'package:networthapp/ui/onboarding_screen.dart';
 import 'package:networthapp/ui/splashscreen.dart';
 
@@ -6,10 +7,12 @@ class RouteNames {
   //Custom class that holds all the named routes as constants for easy reference
   static const String splashScreen = '/';
   static const String onboarding = 'onboarding';
+  static const String addItemScreen = 'addItemScreen';
 
   //Routes and their respective Pages.
   static Map<String, Widget Function(BuildContext)> routes = {
     RouteNames.splashScreen: (context) => SplashScreen(),
     RouteNames.onboarding: (context) => OnboardingScreen(),
+    RouteNames.addItemScreen: (context) => AddItemScreen(),
   };
 }

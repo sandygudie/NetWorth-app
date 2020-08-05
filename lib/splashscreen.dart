6e585ef59a25301c233bcb8fe29d87e59fd7c84d
Rhemi7 <adelefatai@yahoo.com>
2020-08-05 02:47:02 +0100

    Fonts and assets added

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -26,7 +26,7 @@ class _SplashScreenState extends State<SplashScreen> {
 
   initScreen(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFF4545C5),
+      backgroundColor: Theme.of(context).splashColor,
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
--- lib/ui/Theme/appTheme.dart
+++ lib/ui/Theme/appTheme.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 ThemeData appThemeData = ThemeData(
-  primaryColorDark: Color(0xFF121212),
-  primaryColor: Color(0xFF3645C7),
-  backgroundColor: Color(0XFFFFFFFF),
-  buttonColor: Color(0xFF5454D4),
-);
+    primaryColorDark: Color(0xFF121212),
+    primaryColor: Color(0xFF3645C7),
+    backgroundColor: Color(0XFFFFFFFF),
+    buttonColor: Color(0xFF5454D4),
+    splashColor: Color(0xFF4545C5));
